a639d38eeec551ef312286ec6fa373014292d8b7
Syed Usman <susman@dincloud.com>
2019-09-10 00:29:47 +0500

    "First Commit on Case Study"

--- user_managment/constants.py
+++ user_managment/constants.py
@@ -7,7 +7,7 @@ RESPONSE_STATUS = "status"
 RESPONSE_MESSAGE = "message"
 RESPONSE_DATA = "response"
 DEFAULT_ERROR_MESSAGE = "There is some issue your request cannot be processed."
-ERROR_PARAMS_MISSING_TEXT = "Params are missing, Please check API Doc http://example.org"
+ERROR_PARAMS_MISSING_TEXT = "Params are missing, Please check API Doc https://github.com/SyedOsman33/Case-Study/blob/master/API-usage-DOC.md"
 SUCCESS = "success"
 ERROR = "error"
 
